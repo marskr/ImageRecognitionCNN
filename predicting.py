ead248438a3c820eac8ca66c94cd709a178296fc
marskr <marskr@opoczta.pl>
2017-10-25 16:10:08 +0200

    basic python scripts added

---
+++ predicting.py
@@ -0,0 +1,68 @@
+from keras.preprocessing.image import ImageDataGenerator
+# import sequential model type
+from keras.models import Sequential
+# import CNN layers from Keras. Efficiently train on image data by these module:
+from keras.layers import Conv2D, MaxPooling2D
+# import "core" layers from Keras:
+from keras.layers import Activation, Dropout, Flatten, Dense
+from keras import backend as K
+from keras.models import model_from_json
+import cv2
+import numpy as np
+
+# dimensions of our images.
+img_width, img_height = 150, 150
+json_name, h5_name = "modelCNN.json", "modelCNN.h5"
+pic_sample_dir = "dog.1569.jpg"
+list_of_animals = ["dogs", "cats"]
+
+
+class MachineLearningModel:
+
+    def __init__(self, img_width, img_height, json_name, h5_name, pic_sample_dir):
+        self.img_width = img_width
+        self.img_height = img_height
+        self.json_name = json_name
+        self.h5_name = h5_name
+        self.pic_sample_dir = pic_sample_dir
+
+    def loadDataModel(self):
+        json_file = open(json_name, 'r')
+        loaded_model_json = json_file.read()
+        json_file.close()
+        loaded_model = model_from_json(loaded_model_json)
+
+        # load weights into new model
+        loaded_model.load_weights(h5_name)
+        print("Loaded model from disk")
+
+        # evaluate loaded model on test data
+        loaded_model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+
+        print(pic_sample_dir + ":")
+
+        img = cv2.imread(pic_sample_dir)
+        img = cv2.resize(img, (img_width, img_height))
+        img = np.reshape(img, [1, img_width, img_height, 3])
+
+        classes = loaded_model.predict_classes(img)
+        return classes
+
+
+def print_animal(argument):
+    if argument == 0:
+        print_animal("It's a dog!")
+    elif argument == 1:
+        print_animal("It's a cat!")
+    else:
+        print_animal("ERROR!")
+
+MLM = MachineLearningModel(img_width, img_height, json_name, h5_name, pic_sample_dir)
+classes = MLM.loadDataModel()
+if classes[0] == 1:
+    print("It's a cat!")
+else:
+    print("It's a dog!")
+
+
+
---
+++ training.py
@@ -0,0 +1,115 @@
+from keras.preprocessing.image import ImageDataGenerator
+# import sequential model type
+from keras.models import Sequential
+# import CNN layers from Keras. Efficiently train on image data by these module:
+from keras.layers import Conv2D, MaxPooling2D
+# import "core" layers from Keras:
+from keras.layers import Activation, Dropout, Flatten, Dense
+from keras import backend as K
+from keras.models import model_from_json
+import cv2
+import numpy as np
+
+# dimensions of our images.
+img_width, img_height = 150, 150
+
+train_data_dir = 'data/train'
+validation_data_dir = 'data/validation'
+nb_train_samples = 2000
+nb_validation_samples = 1000
+epochs = 2
+batch_size = 16
+json_name = "modelCNN.json"
+h5_name = "modelCNN.h5"
+
+
+class MachineLearningModel:
+
+    def __init__(self, img_width, img_height, json_name, h5_name, train_data_dir, validation_data_dir,
+                       nb_train_samples, nb_validation_samples, epochs, batch_size):
+        self.img_width = img_width
+        self.img_height = img_height
+        self.json_name = json_name
+        self.h5_name = h5_name
+        self.train_data_dir = train_data_dir
+        self.validation_data_dir = validation_data_dir
+        self.nb_train_samples = nb_train_samples
+        self.nb_validation_samples = nb_validation_samples
+        self.epochs = epochs
+        self.batch_size = batch_size
+
+    def trainDataModel(self):
+        if K.image_data_format() == 'channels_first':
+            input_shape = (3, img_width, img_height)
+        else:
+            input_shape = (img_width, img_height, 3)
+
+        # declaration of a sequential model format:
+        model = Sequential()
+
+        # declaration of the input layer:
+        model.add(Conv2D(32, (3, 3), input_shape=input_shape))
+        # the shape of 1 sample (depth, width, height) of each digit image (in our case (3, 150, 150)
+        model.add(Activation('relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+
+        model.add(Conv2D(32, (3, 3)))
+        model.add(Activation('relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+
+        model.add(Conv2D(64, (3, 3)))
+        model.add(Activation('relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+
+        model.add(Flatten())
+        model.add(Dense(64))
+        model.add(Activation('relu'))
+        model.add(Dropout(0.5))
+        model.add(Dense(1))
+        model.add(Activation('sigmoid'))
+
+        model.compile(loss='binary_crossentropy',
+                      optimizer='rmsprop',
+                      metrics=['accuracy'])
+
+        # this is the augmentation configuration we will use for training
+        train_datagen = ImageDataGenerator(
+            rescale=1. / 255,
+            shear_range=0.2,
+            zoom_range=0.2,
+            horizontal_flip=True)
+
+        # this is the augmentation configuration we will use for testing:
+        # only rescaling
+        test_datagen = ImageDataGenerator(rescale=1. / 255)
+
+        train_generator = train_datagen.flow_from_directory(
+            train_data_dir,
+            target_size=(img_width, img_height),
+            batch_size=batch_size,
+            class_mode='binary')
+
+        validation_generator = test_datagen.flow_from_directory(
+            validation_data_dir,
+            target_size=(img_width, img_height),
+            batch_size=batch_size,
+            class_mode='binary')
+
+        model.fit_generator(
+            train_generator,
+            steps_per_epoch=nb_train_samples // batch_size,
+            epochs=epochs,
+            validation_data=validation_generator,
+            validation_steps=nb_validation_samples // batch_size)
+
+        model_json = model.to_json()
+        with open(json_name, "w") as json_file:
+            json_file.write(model_json)
+        print("saved model in json format: " + json_name)
+
+        model.save_weights(h5_name)
+        print("saved model to: " + h5_name)
+
+MLM = MachineLearningModel(img_width, img_height, json_name, h5_name, train_data_dir, validation_data_dir,
+                           nb_train_samples, nb_validation_samples, epochs, batch_size)
+MLM.trainDataModel()
